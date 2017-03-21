e3ea5c1c7f9f0d0fb122e500aa01ef32e72f04fb
Jackie Luc <jackie_luc@hotmail.com>
2017-03-21 17:16:42 -0600

    working symmetrical function using p5js

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-#NetArt
-Net Art
+# Symmetry
+
+## Art piece created by Jackie Luc
+
+___
+
+*Symmetry* is a visual art project that I created using HTML, CSS, and Javascript - primarily using the Javascript library [p5.js](http://p5js.org/). My goal with this project was to explore a new technology that can assist in creating a visually appealing art piece that could be showcased on the web. I was largely inspired by how nature creates such beautiful symmetric structures and thought that Mathematics would also create unique and interesting structures.
+
+___
+
+**Inspiration for this project can be found at [How to Create Art with Mathematics](https://www.quantamagazine.org/20151008-symmetry-math-curves-puzzle/).**
--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,47 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-
+var r;
+var dt;
+var index;
+var params = [
+    [1, 2, 1, 2],
+    
+];
 
 function setup() {
+    createCanvas(800, 800);
 
+    // Initialize all values
+    index = 0;
+    r = height/4.5;
+    dt = PI / 2880;
+}
+    
+function draw() {
+    plot_parametric();
 }
 
-function draw() {
+function plot_parametric() {
+        // Translate the origin point to the center of the screen
+    translate(width/2, height/2);
+    var x0;
+    var x1;
+    var y0;
+    var y1;
   
+    background(255);
+    stroke(0);
+    strokeWeight(3);
+    x0 = 0;
+    y0 = 0;
+    
+    for(var t = dt; t < TWO_PI; t += dt) {
+//        x1 = cos(1*t)-pow(cos(2*t), 3);
+//        y1 = sin(1*t)-pow(sin(2*t), 3);
+        x1 = cos(t)+cos(6*t) / 2+sin(14*t)/3;
+        y1 = sin(t)+sin(6*t) / 2+cos(14*t)/3;
+        if (t > dt)
+            line(r*x0, -r*y0, r*x1, -r*y1);
+        x0 = x1;
+        y0 = y1;
+    }
+    fill(0);
 }
